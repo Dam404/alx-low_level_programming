aaac3339ce1da2b66cdc70cf637109f7bb3eddc4
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2023-12-20 11:16:24 +0100

    Write a program that prints the alphabet in lowercase, followed by a new line. You can only use the putchar function (every other function (printf, puts, etc…) is forbidden) All your code should be in the main function You can only use putchar twice in your code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - The starting point of my program
+*
+*Return: (0) ALWAYS SUCCESSFUL
+*/
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+{
+putchar(alphabet);
+}
+putchar('\n');
+
+return (0);
+}
+
