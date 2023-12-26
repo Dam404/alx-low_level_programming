f926ead9279747ae55ac40f6f0de1418c9e938d2
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2023-12-27 00:23:32 +0100

    Write a function that prints the sign of a number.
    Returns 1 and prints + if n is greater than zero
    Returns 0 and prints 0 if n is zero
    Returns -1 and prints - if n is less than zero
    updated my file by clearing whitespace trailing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 *print_sign - prints the sign of the number.
 *@n: the number of which the sign will be printed.
 *
-*Return:(1) and print + if n is greater than 0, 
+*Return:(1) and print + if n is greater than 0,
 *and return (-1) and print - if n is less than zero.
 *return (0) and print 0 if n is zero
 */
