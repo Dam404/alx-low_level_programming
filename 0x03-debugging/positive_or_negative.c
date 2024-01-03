ccf1d99a44938e6ccd45c61772549b8d9743a2da
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-01-03 01:25:07 +0100

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ *positive_or_negative - checks the code either positive or negative
+ */
+void positive_or_negative(int i)
+{
+
+  if (i == 98)
+    {
+      printf("%d is positive", i);
+    }
+  else if (i < 98)
+    {
+      printf("%d is negative", i);
+    }
+  else
+    {
+      printf("%d is zero", i);
+    }
+  printf("\n");
+  
+}
