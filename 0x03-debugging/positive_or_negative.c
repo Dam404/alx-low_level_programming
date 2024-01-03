6b56e0fef5c906990fbbc5f8555807aa6225183c
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-01-03 01:41:03 +0100

    Commentted out part of the code that is causing the output to go into an infinite loop.

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,23 +2,23 @@
 #include <time.h>
 #include "main.h"
 /**
- *positive_or_negative - checks the code either positive or negative
- */
+*positive_or_negative - checks the code either positive or negative
+*/
 void positive_or_negative(int i)
 {
 
-  if (i == 98)
-    {
-      printf("%d is positive", i);
-    }
-  else if (i < 98)
-    {
-      printf("%d is negative", i);
-    }
-  else
-    {
-      printf("%d is zero", i);
-    }
-  printf("\n");
-  
+if (i == 98)
+{
+printf("%d is positive", i);
+}
+else if (i < 98)
+{
+printf("%d is negative", i);
+}
+else
+{
+printf("%d is zero", i);
+}
+printf("\n");
+
 }
