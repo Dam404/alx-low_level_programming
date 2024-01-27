62af2fcb24517af8f341212150d7939684520481
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-01-27 23:17:31 +0100

    This function  returns the factorial of a given number. If n is lower than 0, the function should return -1 to indicate an error Factorial of 0 is 1

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* factorial - Returns the factorial of a given number
+* @n: The number to compute the factorial of
+*
+* Return: The factorial of n, or -1 if n is negative
+*/
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
+else
+return (n * factorial(n - 1));
+}
