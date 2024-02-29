1cc1fca351cc2df684f2002ed345df5f87554022
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-02-29 21:41:58 +0100

    Updated my code

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day > 28)
+if (month == 2 && day >= 28)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
