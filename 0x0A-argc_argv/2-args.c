70fdf7412623fb5ba8ca514e0a9dbc65614f6b89
Odeyemi Damilola <odeyemidamilola078@gmail.com>
2024-05-14 22:37:22 +0100

    Updated my code on C, argc and argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,17 @@
+/**
+* main - This is the entry point of my program, this prints all it receives.
+*@argc: The number of command-line arguments.
+*@argv: An array containing the command-line arguments.
+*All arguments are printed, including the first one
+*And it-s printed one argument per line, ending with a new line.
+*Return: 0 (Indicates successful program)
+*/
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 	printf("%s\n", argv[i]);
